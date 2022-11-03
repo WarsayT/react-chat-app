2bd2548fe636524e71b09d693ea62cb9b8bf4140
WarsayT <warsay.teklebrhan@gmail.com>
2022-11-04 03:26:12 +0400

    new components, message feed built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 /* eslint-disable no-unused-vars */
 
-// import './App.css';
+import './App.css';
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
-    <ChatEngine />
+    <ChatEngine
+      height="100vh"
+      projectID="c8769ad7-1b54-4ea6-b226-5c5f539958cc"
+      userName="WarsayT"
+      userSecret="wars123"
+      renderChatFeed={(chatAppProps) =>  <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
